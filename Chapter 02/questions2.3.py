baf89340ed8e48a075cf94bcd475ac41a3065651
lee-leonardo <lee.leonardo.king@gmail.com>
2021-11-15 22:08:26 -0800

    2.3 p 1

---
+++ Chapter 02/questions2.3.py
@@ -0,0 +1,33 @@
+from matplotlib.patches import Polygon
+from matplotlib.pyplot import grid
+from vectors import length
+
+# 2.2.27
+print(length(-1.34, 2.68))
+
+# 2.2.28
+# tan(22) ~= 0.4
+
+# 2.2.29
+# length = 15, degrees = 37
+# sine (37) = 0.6
+# cosine (37) = 0.8
+# 15 * 0.6 = 9 == b
+# 15 * 0.8 = 12 == a
+#  ~= (12, 9)
+
+# 2.2.30
+# length = 8.5
+# theta = 125
+# sin (125 d) = 0.819
+# cos (125 d) = -0.574
+# y = 6.9615
+# x = -4.879
+
+# 2.2.31
+# sin (0) = 0
+# cos (0) = 1
+# sin (90) = 1
+# cos (90) = 0
+# sin (180) = 0
+# cos (180) = -1

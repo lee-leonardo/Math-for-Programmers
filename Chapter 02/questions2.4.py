d00e493b97ee4df4cefc598a18537e5191d54157
lee-leonardo <lee.leonardo.king@gmail.com>
2021-11-28 10:25:39 -0800

    questions for the section

--- Chapter 02/questions2.4.py
+++ Chapter 02/questions2.4.py
@@ -1,8 +1,34 @@
+from math import pi
+
+from vector_drawing import draw
 from vectors import to_cartesian
 from vectors import to_polar
+from vectors import translate
+from vectors import rotate
 
 # 2.42
 # write a rotate(angle, vectors)
-def rotate(angle, vectors):
-    polars = [to_polar(x) for x in vectors]
-    return [to_cartesian(d, a + angle) for d, a in polars]
+# def rotate(angle, vectors):
+#     polars = [to_polar(x) for x in vectors]
+#     return [to_cartesian(d, a + angle) for d, a in polars]
+
+# 2.43
+# write regular_polygon(n) that returns the cartesian coordinates of an n sided polygon
+def regular_polygon(n):
+    angle = (2 * pi) / n
+    return [to_cartesian((1, angle * i)) for i in range(0,n)]
+
+# 2.44
+# dino_vector translate by (8,8) then rotate by 5*pi/3, what about vv?
+dino_vectors = [(6,4), (3,1), (1,2), (-1,5), (-2,5), (-3,4), (-4,4), 
+    (-5,3), (-5,2), (-2,2), (-5,1), (-4,0), (-2,1), (-1,0), (0,-3), 
+    (-1,-4), (1,-4), (2,-3), (1,-2), (3,-1), (5,1)
+]
+
+draw(
+    dino_vectors,
+    translate((8,8), rotate(5 * pi / 3, dino_vectors)),
+    rotate(5 * pi / 3, translate((8,8), dino_vectors))
+)
+
+# no othey are not the same
